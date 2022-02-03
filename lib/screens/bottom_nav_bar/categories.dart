5215edce347c4f9ae64b61804e241e66a049358b
ZTLAB67 <ztlab67@ZTLAB67.local>
2022-02-03 18:59:35 +0530

    camera and location

--- lib/screens/bottom_nav_bar/categories.dart
+++ lib/screens/bottom_nav_bar/categories.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+import 'package:image_picker/image_picker.dart';
 
 class Categories extends StatefulWidget {
   const Categories({Key? key}) : super(key: key);
@@ -10,9 +17,31 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
+  late var _image;
+
+  Future _pickImageFromCamera() async {
+    var image = await ImagePicker()
+        .pickImage(source: ImageSource.camera, imageQuality: 50);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future _pickImageFromGallery() async {
+    File image1 = (await ImagePicker()
+        .pickImage(source: ImageSource.gallery, imageQuality: 50)) as File;
+    setState(() {
+      _image = image1;
+    });
+  }
+
   List<String> dropdownSublist = ['Kotlin', 'Java', 'Flutter'];
   List<String> dropdownItems = ['Android', 'IOS', 'QA', 'web', 'BA'];
   String? dropdownValue = '';
+
+  final LatLng _initialMapLocation = const LatLng(23.1925450, 72.6131467);
+  late GoogleMapController mapController;
+  final Location _location = Location();
 
   bool isSwitched = false;
 
@@ -26,39 +55,122 @@ class _CategoriesState extends State<Categories> {
     dropdownValue = dropdownItems[0];
   }
 
+  void _onMapCreated(GoogleMapController _cntlr) {
+    mapController = _cntlr;
+    _location.onLocationChanged.listen((location) {
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(location.latitude!, location.longitude!),
+              zoom: 15),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          DropdownButton<String>(
-            value: dropdownValue,
-            style: TextStyle(color: Theme.of(context).primaryColor),
-            borderRadius: BorderRadius.circular(5.0),
-            items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: onDropdownChanged,
-          ),
-          Switch(
-            activeColor: Theme.of(context).primaryColorDark,
-            activeTrackColor: Theme.of(context).primaryColorLight,
-            onChanged: (status) {
-              setState(() {
-                isSwitched = !isSwitched;
-              });
-            },
-            value: isSwitched,
-          ),
-          const LinearProgressIndicator(
-            value: 0.6, //controller.value,
-            semanticsLabel: 'Linear progress indicator',
-          ),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              value: dropdownValue,
+              style: TextStyle(color: Theme.of(context).primaryColor),
+              borderRadius: BorderRadius.circular(5.0),
+              items:
+                  dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: onDropdownChanged,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: const LinearProgressIndicator(
+                value: 0.6, //controller.value,
+                semanticsLabel: 'Linear progress indicator',
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: const AutocompleteBasicName(),
+            ),
+            Expanded(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _initialMapLocation,
+                ),
+                mapType: MapType.normal,
+                myLocationButtonEnabled: true,
+              ),
+            ),
+            Switch(
+              activeColor: Theme.of(context).primaryColorDark,
+              activeTrackColor: Theme.of(context).primaryColorLight,
+              onChanged: (status) {
+                setState(() {
+                  isSwitched = !isSwitched;
+                });
+              },
+              value: isSwitched,
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: _pickImageFromCamera,
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.camera,
+                      ),
+                      title: Text('Image from camera'),
+                    )),
+                ElevatedButton(
+                    onPressed: _pickImageFromGallery,
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.roofing,
+                      ),
+                      title: Text('Image from gallery'),
+                    )),
+              ],
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class AutocompleteBasicName extends StatelessWidget {
+  const AutocompleteBasicName({Key? key}) : super(key: key);
+
+  static const List<String> _userOptions = [
+    'Alice',
+    'Bob',
+    'Charlie',
+  ];
+
+  static String _displayStringForOption(String option) => option;
+
+  @override
+  Widget build(BuildContext context) {
+    return Autocomplete<String>(
+      displayStringForOption: _displayStringForOption,
+      optionsBuilder: (TextEditingValue textEditingValue) {
+        if (textEditingValue.text == '') {
+          return const Iterable<String>.empty();
+        }
+        return _userOptions.where((String option) {
+          return option
+              .toLowerCase()
+              .contains(textEditingValue.text.toLowerCase());
+        });
+      },
     );
   }
 }
